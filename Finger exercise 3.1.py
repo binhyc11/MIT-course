3620d1aa6963af452e37e21966947188e0cb2a5b
binhyc11 <90891898+binhyc11@users.noreply.github.com>
2021-09-18 15:05:37 +0900

    Update Finger exercise 3.1.py

--- Finger exercise 3.1.py
+++ Finger exercise 3.1.py
@@ -24,3 +24,24 @@ while pwr < 6:
     pwr +=1
     if flag == False:
         print('always have pairs')
+
+       
+## version 2:
+
+while pwr < 6:
+    while root ** pwr < abs (num):
+        root += 1
+    if root ** pwr == abs (num):
+        if num == 0:
+            print (root, pwr)
+        elif num < 0 and pwr % 2 != 0:
+            print (-root, pwr)
+        elif num > 0 and pwr % 2 == 0:
+            print (root, - root, pwr)
+        elif num > 0 and pwr % 2 != 0:
+            print (root, pwr)
+        flag = True    
+    root = 0
+    pwr +=1
+    if flag == False:
+        print('always have pairs')
